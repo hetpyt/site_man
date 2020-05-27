c9fa528c0b58740442db9e6a53839f92ee3f6201
hetpyt <43849401+hetpyt@users.noreply.github.com>
2020-05-27 19:00:43 +0300

    php

--- php/template.php
+++ php/template.php
@@ -1,12 +1,3 @@
 <?php
-require_once("main_menu.php");
-
-$root = array_shift($main_menu);
-echo_item_begin('normal', '', $root['title']);
-echo_item_end();
-
-usort($root['items'], 'cmp_menu_items');
-foreach ($root['items'] as $menu_item) {
-    build_menu('', $menu_item);
-}
+    require_once("main_menu.php");
 ?>
